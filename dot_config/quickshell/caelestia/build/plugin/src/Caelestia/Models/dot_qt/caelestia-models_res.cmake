e44b6872b2486100081ad20ab4b3f9357e75b399
vladg00dman <vladislavsunny@gmail.com>
2026-04-13 21:29:58 +0300

    remove quickshell tracking, simplified metapac groups

--- dot_config/quickshell/caelestia/build/plugin/src/Caelestia/Models/dot_qt/caelestia-models_res.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "caelestia-models")
-set(working_dir "/home/vladg00dman/.config/quickshell/caelestia/plugin/src/Caelestia/Models")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/vladg00dman/.config/quickshell/caelestia/build/plugin/src/Caelestia/Models/.qt/caelestia-models_res.txt")
--- dot_config/quickshell/caelestia/build/plugin/src/Caelestia/dot_qt/caelestia_qml.cmake
+++
@@ -1,7 +0,0 @@
-
-set(target "caelestia")
-set(working_dir "/home/vladg00dman/.config/quickshell/caelestia/plugin/src/Caelestia")
-set(src_and_dest_list
-
-)
-set(timestamp_file "/home/vladg00dman/.config/quickshell/caelestia/build/plugin/src/Caelestia/.qt/caelestia_qml.txt")
